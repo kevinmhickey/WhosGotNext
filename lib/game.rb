3fb7019d547399c7df87e5d1b8b03d63fc8b391c
Kevin Hickey <khickey@thoughtworks.com>
2014-03-04 17:53:45 -0600

    Games can now be started

--- lib/game.rb
+++ lib/game.rb
@@ -11,17 +11,25 @@ class Game
 	has n, :players
 
 	def in_progress?
-		players.any? { |player| player.state == :playing }
+		players.any? {|player| player.state == :playing}
 	end
 
 	def players_playing
-		players.find_all { |player| player.state == :playing }
+		players.find_all {|player| player.state == :playing}
 	end
 
 	def game_over!
 		players_playing.each do |player|
 			player.state = :done
 			player.save
+		end
+	end
+	
+	def start!
+		player_count.times do 
+			waiting_player = players.find {|player| player.state == :waiting}
+			waiting_player.state = :playing
+			waiting_player.save
 		end
 	end
 	
--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -61,12 +61,14 @@ describe Game do
 
 		it "should mark all playing players as done when the game is over" do
 			@game.game_over!
+			@game.players_playing.empty?.should eq(true)
 
 			@player1.reload
 			@player2.reload
 
 			@player1.state.should eq(:done)
 			@player2.state.should eq(:done)
+
 		end
 
 		it "should do nothing to waiting players when the game is over" do
@@ -75,6 +77,33 @@ describe Game do
 			@waiter.reload
 
 			@waiter.state.should eq(:waiting)
+		end
+	end
+
+	describe "Starting a game" do
+		before(:each) do 
+			@game = Game.create(:name => "Darts", :player_count => 2, :winner_stays => false)
+			@players = []
+			@players << Player.create(:name => "Kevin", :state => :waiting, :game => @game)
+			@players << Player.create(:name => "Cad", :state => :waiting, :game => @game)
+			@players << Player.create(:name => "Jimmy", :state => :waiting, :game => @game)
+
+			@game.start!
+
+			@players.each { |player| player.reload }
+		end
+
+		it "should set the next two players to playing on game start" do
+			@players[0].state.should eq(:playing)
+			@players[1].state.should eq(:playing)
+		end
+
+		it "should not change the state of other players" do
+			@players[2].state.should eq(:waiting)
+		end
+
+		it "should contain an updated list of players playing" do
+			@game.players_playing.should eq([@players[0], @players[1]])
 		end
 	end
 end
