e5ec3158458ff23d608934ad34a21398340c949c
Kevin Hickey <khickey@thoughtworks.com>
2014-03-04 17:18:54 -0600

    Games can now be ended

--- lib/game.rb
+++ lib/game.rb
@@ -13,4 +13,16 @@ class Game
 	def in_progress?
 		players.any? { |player| player.state == :playing }
 	end
+
+	def players_playing
+		players.find_all { |player| player.state == :playing }
+	end
+
+	def game_over!
+		players_playing.each do |player|
+			player.state = :done
+			player.save
+		end
+	end
+	
 end
--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -7,6 +7,9 @@ describe Game do
 		DataMapper::Model.raise_on_save_failure = true
 		DataMapper.setup(:default, 'sqlite::memory:')
 		DataMapper.finalize
+	end
+
+	before(:each) do
 		DataMapper.auto_migrate!
 	end
 
@@ -25,22 +28,54 @@ describe Game do
 		player2.game.should eq(game)
 	end
 
-	it "should know a game is in progress if any players are playing" do
-		game = Game.create(:name => "Golden Tee", :player_count => 4, :winner_stays => false)
-		player1 = Player.create(:name => "Player 1", :game => game)
-		player2 = Player.create(:name => "Player 2", :state => :playing, :game => game)
-		
-		game.in_progress?.should eq(true)
+	describe "A game in progress" do
+		before(:each) do 
+			@game = Game.create(:name => "Solitare", :player_count => 1, :winner_stays => false)
+			@player1 = Player.create(:name => "Player 1", :game => @game)
+			@player2 = Player.create(:name => "Player 2", :state => :playing, :game => @game)
+		end
+
+		it "should know a game is in progress if any players are playing" do
+			@game.in_progress?.should eq(true)
+		end
+
+		it "should know a game is not in progress if no players are playing" do
+			@player2.state = :done
+			@player2.save
+			
+			@game.in_progress?.should eq(false)
+		end
+
+		it "should list only player 2 as playing" do
+			@game.players_playing.should eq([@player2])
+		end
 	end
 
-	it "should know a game is not in progress if no players are playing" do
-		game = Game.create(:name => "Golden Tee", :player_count => 4, :winner_stays => false)
-		player1 = Player.create(:name => "Player 1", :game => game)
-		player2 = Player.create(:name => "Player 2", :state => :playing, :game => game)
-		player2.state = :done
-		player2.save
-		
-		game.in_progress?.should eq(false)
+	describe "Stopping a game" do
+		before(:each) do 
+			@game = Game.create(:name => "Darts", :player_count => 2, :winner_stays => false)
+			@player1 = Player.create(:name => "Player 1", :state => :playing, :game => @game)
+			@player2 = Player.create(:name => "Player 2", :state => :playing, :game => @game)
+			@waiter = Player.create(:name => "Waiter", :state => :waiting, :game => @game)
+		end
+
+		it "should mark all playing players as done when the game is over" do
+			@game.game_over!
+
+			@player1.reload
+			@player2.reload
+
+			@player1.state.should eq(:done)
+			@player2.state.should eq(:done)
+		end
+
+		it "should do nothing to waiting players when the game is over" do
+			@game.game_over!
+
+			@waiter.reload
+
+			@waiter.state.should eq(:waiting)
+		end
 	end
 end
 
