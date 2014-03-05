b2a8f848bcbf6abe8a3d247b2458610e92d0d6b4
Kevin Hickey <khickey@thoughtworks.com>
2014-03-05 16:35:43 -0600

    Started adding game playing elements to game view

--- whosgotnext.rb
+++ whosgotnext.rb
@@ -4,7 +4,7 @@ require 'lib/game'
 require 'lib/player'
 
 configure :development do 
-	DataMapper.setup(:default, 'sqlite::memory:')
+	DataMapper.setup(:default, 'sqlite:gotnext.db')
 	DataMapper.finalize
 	DataMapper.auto_migrate!
 end
@@ -27,5 +27,11 @@ end
 
 get '/game/show/:id' do
 	@game = Game.get(params[:id])
+	erb :game
+end
+
+post '/game/start/:id' do
+	@game = Game.get(params[:id])
+	@game.start!
 	erb :game
 end
